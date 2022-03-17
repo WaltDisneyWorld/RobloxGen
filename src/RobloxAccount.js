06222d3c780f672b46a5106fb9b0969ba8eca89e
Jose Martinez <devbrxen@gmail.com>
2022-03-17 16:32:25 -0400

    move code to src dir

---
+++ src/RobloxAccount.js
@@ -0,0 +1,13 @@
+module.exports = class RobloxAccount {
+    username
+    userId
+    password
+    cookie
+
+    constructor(username, userId, password, cookie) {
+        this.username = username;
+        this.userId = userId;
+        this.password = password;
+        this.cookie = cookie;
+    }
+}
